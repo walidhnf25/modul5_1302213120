e924bb97f40205bf8b3f8efd46bc5109c89bc274
PC7-IFLAB <walidhnf12@gmail.com>
2023-03-21 16:17:22 +0700

    implementasi-generic-method

--- modul5_1302213120/Program.cs
+++ modul5_1302213120/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices.ComTypes;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,6 +11,17 @@ namespace modul5_1302213120
     {
         static void Main(string[] args)
         {
+            Penjumlahan penjumlahan= new Penjumlahan();
+            Console.WriteLine(penjumlahan.JumlahTigaAngka<long>(13, 2, 21));
+        }
+    }
+
+    public class Penjumlahan
+    {
+        public T JumlahTigaAngka<T>( T lhs,  T rhs, T xhs)
+        {
+            return (dynamic)lhs+(dynamic)rhs+(dynamic)xhs;
+
         }
     }
 }
